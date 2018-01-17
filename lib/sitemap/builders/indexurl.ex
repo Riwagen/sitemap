94c5389233b63c7f37da263e4156186b58c701c6
Max <artmartfrontend@gmail.com>
2018-01-17 10:26:10 +0200

    comment lastmod

--- lib/sitemap/builders/indexurl.ex
+++ lib/sitemap/builders/indexurl.ex
@@ -5,7 +5,7 @@ defmodule Sitemap.Builders.Indexurl do
   def to_xml(link, opts \\ []) do
     element(:sitemap, Funcs.eraser([
       element(:loc, if(opts[:host], do: Funcs.urljoin(link, opts[:host]), else: link)),
-      element(:lastmod, Keyword.get_lazy(opts, :lastmod, fn -> Funcs.iso8601 end))
+      # element(:lastmod, Keyword.get_lazy(opts, :lastmod, fn -> Funcs.iso8601 end))
     ]))
   end
 
